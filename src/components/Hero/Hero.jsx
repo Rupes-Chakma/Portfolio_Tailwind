67f61ecbae7a64f641f6a17308bfe1d69831713d
Rupes-Chakma <chakmaworld.cw@gmaiol.com>
2026-01-15 11:21:41 +0600

    side icon bar adding...

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./Hero.css";
 import Button from "../../utils/Button";
 import { RxArrowTopRight } from "react-icons/rx";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
 const Hero = () => {
   return (
-    <div className="container">
-      <div className="grid grid-cols-2 items-center">
-        <div>
+    <div className="container py-4 lg:py-0">
+      <div className="lg:grid grid-cols-2 items-center">
+        <div className="">
           <h3 className="heading font-inter font-medium text-[20px] leading-[140%] text-[#0DB720] mb-4">
             Welcome
           </h3>
-          <h1 className="font-inter font-semibold text-[58px] leading-[120%] text-[#000]">
+          <h1 className="font-inter font-semibold text-[35px] lg:text-[58px] leading-[120%] text-[#000]">
             I have <span className="text-[#0DB720]">Creative Design </span>
             Experience
           </h1>
@@ -32,9 +36,21 @@ const Hero = () => {
           </div>
         </div>
 
-        <div>
-          <img src="/hero_man.png" alt="hero" />
-          <p>i am rupes</p>
+        <div className="relative  lg:flex justify-end space-x-10">
+          <div className="flex justify-center lg:justify-end mt-5 lg:mt-0">
+            <img src="/hero_man.png" alt="hero" />
+          </div>
+          <div className="lg:absolute lg:top-0 lg:-right-30 h-full ">
+            <div className="flex justify-start gap-4 lg:rotate-90 mt-5 lg:mt-[100%] items-center  text-[#0DB720] ">
+              <p className="text-[18px]">Follow me on :</p>
+              <div className="flex gap-4 text-[20px]">
+                <FaFacebookSquare />
+                <FaInstagramSquare />
+                <FaLinkedin />
+                <FaGithubSquare />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/utils/Button.jsx
+++ src/utils/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ style, title, arrow }) => {
   return (
     <div>
-      <button className={`items-center gap-2 flex ${style}`}>
+      <button className={`items-center gap-4 flex ${style}`}>
         {title}
         {arrow}
       </button>
